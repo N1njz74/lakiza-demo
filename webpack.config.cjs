0307c6d1c7cd435f4bbccc99e94d2f7a32ce7c3f
N1njz74 <promalpinist74@yandex.ru>
2026-05-20 05:05:57 -0700

    Add GitHub Actions workflow for NodeJS with Webpack (#1)
    * Add GitHub Actions workflow for NodeJS with Webpack
    
    * Migrate package scripts to Webpack
    
    * Remove Vite entry script from HTML
    
    * Add Webpack configuration
    
    * Add Babel configuration for React
    
    * Use project build command in Webpack workflow
    
    * Remove Vite configuration

---
+++ webpack.config.cjs
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/main.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'assets/[name].[contenthash].js',
+      publicPath: isProduction ? '/lakiza-demo/' : '/',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              },
+            },
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp|ico|woff2?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[hash][ext][query]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+    ],
+    devServer: {
+      port: 5173,
+      hot: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    },
+  };
+};
