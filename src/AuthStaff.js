e4b6bd523aa36093bab6191d0e4b30bc94f71f54
N1njz74 <promalpinist74@yandex.ru>
2026-05-20 06:53:15 -0700

    Add demo staff credentials

---
+++ src/AuthStaff.js
@@ -0,0 +1,6 @@
+export const EXTRA_STAFF_USERS = [
+  { login: 'kristina', password: 'kristina123', role: 'admin', name: 'Кристина', surname: 'Лакиза' },
+  { login: 'vera', password: 'vera123', role: 'therapist', name: 'Вера', surname: 'Соколова' },
+  { login: 'alina', password: 'alina123', role: 'therapist', name: 'Алина', surname: 'Миронова' },
+  { login: 'natalia', password: 'natalia123', role: 'therapist', name: 'Наталья', surname: 'Орлова' },
+];
