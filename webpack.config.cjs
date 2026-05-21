9432f4fd1a15a1cce130087e09a9d925a7150d76
N1njz74 <promalpinist74@yandex.ru>
2026-05-21 00:50:16 -0700

    Load logo patch as global entry

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,7 +5,7 @@ module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
   return {
-    entry: ['./src/marketing-main.jsx', './src/calendarEnhance.js'],
+    entry: ['./src/marketing-main.jsx', './src/calendarEnhance.js', './src/logoPatch.js'],
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'assets/[name].[contenthash].js',
