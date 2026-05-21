f7e3ba36421b4acb9eb9c2a4ad9cecd48227b32b
N1njz74 <promalpinist74@yandex.ru>
2026-05-20 23:51:07 -0700

    Load mobile vertical hero patch

--- src/marketing-main.jsx
+++ src/marketing-main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AuthMarketingApp from './AuthMarketingApp.jsx';
+import './mobileHeroPatch.js';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
