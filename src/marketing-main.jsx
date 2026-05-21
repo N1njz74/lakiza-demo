f572440a511c25141a19e8a591d09a2964ea3886
N1njz74 <promalpinist74@yandex.ru>
2026-05-21 00:32:58 -0700

    Load generated moss logo patch

--- src/marketing-main.jsx
+++ src/marketing-main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AuthMarketingApp from './AuthMarketingApp.jsx';
 import './mobileHeroPatch.js';
+import './logoPatch.js';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
